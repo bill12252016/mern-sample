c8dedf4469838cd3d69e9a89b2c25eb0c8041013
Gary <gear@thytruth.com>
2017-09-14 02:22:01 -0700

    testing change new string 4E95KyRq

--- README.md
+++ README.md
@@ -100,3 +100,4 @@ RNwiIl6q
 ZSDSpzLB
 mKj9spwe
 iRADf23j
+4E95KyRq
--- client/modules/App/components/Header/Header.js
+++ client/modules/App/components/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => (
         <div className={`${fbStyles.row}`}>
           <div className={styles['header-copy']}>
             <h1>Magic Blog</h1>
-            <h2>iRADf23j</h2>
+            <h2>4E95KyRq</h2>
           </div>
         </div>
       </div>
