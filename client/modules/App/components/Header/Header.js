ea86e3deb20b4331a694139b81c8454df1b63a91
Gary <gear@thytruth.com>
2017-09-13 18:20:15 -0700

    testing change new string 04I9zTB0

--- client/modules/App/components/Header/Header.js
+++ client/modules/App/components/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => (
         <div className={`${fbStyles.row}`}>
           <div className={styles['header-copy']}>
             <h1>Magic Blog</h1>
-            <h2>RNwiIl6q</h2>
+            <h2>04I9zTB0</h2>
           </div>
         </div>
       </div>
--- dist/server.bundle.js
+++ dist/server.bundle.js
@@ -3288,7 +3288,7 @@
 
 	var _ref = _jsx('h1', {}, void 0, 'Magic Blog');
 
-    var _ref2 = _jsx('h2', {}, void 0, 'RNwiIl6q');
+    var _ref2 = _jsx('h2', {}, void 0, '04I9zTB0');
 
 	var Header = function Header() {
 	  return _jsx(_reactParallax.Parallax, {
