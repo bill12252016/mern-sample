5eeb0cf46e01351e029a33263ebefd1e158049a1
Gary <gear@thytruth.com>
2017-10-03 03:45:26 -0700

    testing change new string rGBUweU5

--- dist/server.bundle.js
+++ dist/server.bundle.js
@@ -3288,7 +3288,7 @@
 
 	var _ref = _jsx('h1', {}, void 0, 'Magic Blog');
 
-    var _ref2 = _jsx('h2', {}, void 0, '2gM2FAtH');
+    var _ref2 = _jsx('h2', {}, void 0, 'rGBUweU5');
 
 	var Header = function Header() {
 	  return _jsx(_reactParallax.Parallax, {
