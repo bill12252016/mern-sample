7e7af077ddf1670f203bdd15933565d75b9cfc0d
Gary <gary@elasticdev.io>
2019-09-13 17:57:58 +0800

    testing change new string 5Xya0n9M

--- dist/server.bundle.js
+++ dist/server.bundle.js
@@ -3288,7 +3288,7 @@
 
 	var _ref = _jsx('h1', {}, void 0, 'Magic Blog');
 
-    var _ref2 = _jsx('h2', {}, void 0, 'NbJDPayY');
+    var _ref2 = _jsx('h2', {}, void 0, '5Xya0n9M');
 
 	var Header = function Header() {
 	  return _jsx(_reactParallax.Parallax, {
