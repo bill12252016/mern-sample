78f2eb40fc953a1a89ff33d877641c06f2e49081
Gary <gary@elasticdev.io>
2019-09-16 18:18:19 +0800

    testing change new string QtVIM71m

--- dist/server.bundle.js
+++ dist/server.bundle.js
@@ -3288,7 +3288,7 @@
 
 	var _ref = _jsx('h1', {}, void 0, 'Magic Blog');
 
-    var _ref2 = _jsx('h2', {}, void 0, 'D7fz4MUi');
+    var _ref2 = _jsx('h2', {}, void 0, 'QtVIM71m');
 
 	var Header = function Header() {
 	  return _jsx(_reactParallax.Parallax, {
