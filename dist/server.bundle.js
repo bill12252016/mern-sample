c59daa4b1299bba94b631bfc8441dd471fa610e2
Gary <gary@elasticdev.io>
2019-09-16 20:49:31 +0800

    testing change new string NR85NvYE

--- dist/server.bundle.js
+++ dist/server.bundle.js
@@ -3288,7 +3288,7 @@
 
 	var _ref = _jsx('h1', {}, void 0, 'Magic Blog');
 
-    var _ref2 = _jsx('h2', {}, void 0, '7qAKmt1C');
+    var _ref2 = _jsx('h2', {}, void 0, 'NR85NvYE');
 
 	var Header = function Header() {
 	  return _jsx(_reactParallax.Parallax, {
