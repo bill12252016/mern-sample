011999a2810980048ea7c14c506807b50d323ec2
Gary <gary@elasticdev.io>
2019-09-17 10:17:20 +0800

    testing change new string AGhkWSVD

--- client/modules/App/components/Header/Header.js
+++ client/modules/App/components/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => (
         <div className={`${fbStyles.row}`}>
           <div className={styles['header-copy']}>
             <h1>Magic Blog</h1>
-            <h2>6KsfFTdS</h2>
+            <h2>AGhkWSVD</h2>
           </div>
         </div>
       </div>
